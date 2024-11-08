a036d9400597309d581ec821bec6f37f5f6712c8
Nikhil Raj <94313877+dudenrb@users.noreply.github.com>
2024-11-09 00:02:26 +0530

    Update major.py

--- major.py
+++ major.py
@@ -8,6 +8,12 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
 import screen_brightness_control as sbcontrol
+import sys
+
+run_program = input("Do you want to run the Gesture Controller program? (y/n): ").strip().lower()
+if run_program != 'y':
+    print("Program exited.")
+    sys.exit()
 
 pyautogui.FAILSAFE = False
 mp_drawing = mp.solutions.drawing_utils
